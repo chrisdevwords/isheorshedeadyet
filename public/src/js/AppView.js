1db4b1515adfa26394854964e56bf19b81dc4b99
chrisdevwords <chrisdevwords@yahoo.com>
2015-02-28 13:50:15 -0500

    front end sort of working

--- public/src/js/AppView.js
+++ public/src/js/AppView.js
@@ -1,5 +1,6 @@
 'use strict';
 
+var _ = require('underscore');
 var Backbone = require('backbone');
 
 module.exports = Backbone.View.extend({
@@ -11,7 +12,47 @@ module.exports = Backbone.View.extend({
     },
 
     initialize : function (options) {
-        this.$('.content-wrap').html('<p>is this thing on?</p>');
+
+        var _this = this;
+        var msg;
+
+        if (_.isEmpty(options.sv.name)) {
+            _this.$('.content-wrap').html('<p>show form.</p>');
+            return;
+        }
+
+        $.ajax({
+            type: "GET",
+            url: "/api/?name=" + options.sv.name,
+            contentType: "application/json; charset=utf-8",
+            async: false,
+            dataType: "json",
+            success: function (resp, textStatus, jqXHR) {
+                console.log(resp.data);
+                if (resp.data.wikiResp) {
+                    msg = resp.data.wikiResp.title;
+                    if (resp.data.isCelebrity) {
+                        if (resp.data.isDead) {
+                            msg += ' is fucking dead.'
+                        } else {
+                            msg += ' is not dead.'
+                        }
+                    } else {
+                        msg += ' is not a celebrity. Who cares?'
+                    }
+                } else {
+                    msg = 'show error.';
+                }
+                _this.$('.content-wrap').html('<p>' + msg + '</p>');
+
+            },
+            error: function (errorMessage) {
+                _this.$('.content-wrap').html('<p>show error.</p>');
+            }
+        });
+
+
+
     }
 
 
--- public/src/js/main.js
+++ public/src/js/main.js
@@ -10,45 +10,8 @@ var AppView = require('./AppView');
 var app = module.exports = {};
 
 $(function () {
-
     var SV = root.__SERVER_VARS__ || {};
-    var title = (SV.name || '').split(' ').join('_');
-
     app.view = new AppView({sv:SV});
-
-    $.ajax({
-        type: "GET",
-        url: "http://en.wikipedia.org/w/api.php?action=parse&format=json&prop=text&section=0&page=" + title + "&callback=?",
-        contentType: "application/json; charset=utf-8",
-        async: false,
-        dataType: "json",
-        success: function (data, textStatus, jqXHR) {
-            console.log(data);
-            var markup;
-            var $blurb;
-            if (data.parse) {
-                markup = data.parse.text["*"];
-                $blurb = $('<div></div>').html(markup);
-                if (!$blurb.find('.bday').length) {
-                    alert('not a celebrity...');
-                } else {
-                    if ($blurb.find('.deathdate').length) {
-                        alert('dead');
-                    } else {
-                        alert('not dead');
-                    }
-                }
-
-            } else {
-                alert('no name provided');
-            }
-
-            console.log(data);
-            console.log($blurb);
-        },
-        error: function (errorMessage) {
-        }
-    });
     window.app = app;
 });
 
