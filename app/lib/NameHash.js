86158695a0a471df76a46dd2db1ef40f8d04d75f
chrisdevwords <chrisdevwords@yahoo.com>
2015-03-01 08:17:52 -0500

    adding some more celebrity whitelist hacks... inside joke.

--- app/lib/NameHash.js
+++ app/lib/NameHash.js
@@ -2,5 +2,7 @@
 
 module.exports = {
     'madonna' : 'Madonna_(singer)',
-    'sting'   : 'Sting_(musician)'
+    'sting'   : 'Sting_(musician)',
+    'dio'     : 'Ronnie James Dio',
+    'john legend' : 'John Lennon'
 };
--- app/routes/index.js
+++ app/routes/index.js
@@ -3,13 +3,15 @@
 var express = require('express');
 var router  = express.Router();
 var Wikipedia = require('../lib/Wikipedia');
+var whitelist = require('../lib/NameHash');
 var _ = require('underscore');
 
 /* GET home page */
 router.get('/', function (req, res) {
 
-    var name = req.query.name;
+    var name = req.query.name || '';
     var search = new Wikipedia();
+    name = whitelist[name.toLowerCase()] || name;
     if (_.isEmpty(name)) {
         res.render('index', {
             serverVars : {}
