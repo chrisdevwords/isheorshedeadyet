154d2848d25bad094b664e965bd2e099ff840ef3
chrisdevwords <chrisdevwords@yahoo.com>
2015-03-02 08:04:06 -0500

    adding prince and co to the name hash...

--- app/lib/NameHash.js
+++ app/lib/NameHash.js
@@ -3,6 +3,9 @@
 module.exports = {
     'madonna' : 'Madonna_(singer)',
     'sting'   : 'Sting_(musician)',
+    'prince'  : 'Prince_(musician)',
+    'apollonia' : 'Apollonia_Kotero',
+    'vanity'    : 'Vanity_(singer)',
     'dio'     : 'Ronnie James Dio',
     'john legend' : 'John Lennon'
 };
